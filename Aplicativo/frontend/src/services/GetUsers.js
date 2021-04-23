bfe71f37abb40741edb0ea294e9c82236bd676e3
gabriel-reis-silva <gabriel.reis1945@gmail.com>
2021-04-23 14:29:29 -0300

    verificaçao de status para login usando xmlhttp request

--- Aplicativo/frontend/src/services/GetUsers.js
+++ Aplicativo/frontend/src/services/GetUsers.js
@@ -1,9 +1,24 @@
 const getBtn = document.getElementById('getBtn');
+const area = document.getElementById('area');
+
 
 
 const getData = () => {
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:8080/usuarios');
+    xhr.open('GET', `http://localhost:8080/usuarios/login/${email}/${senha}`);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+        // const resposta = JSON.parse(xhr.response);
+        const resposta = xhr.response;
+        console.log(xhr.status);       
+        if(xhr.status == 200) {
+console.log("foi")
+        }else{
+            console.log("ruim")
+        }
+    };
     xhr.send();
 
 };
