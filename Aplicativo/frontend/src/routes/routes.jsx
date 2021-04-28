76c19d11269567c792deb058848bc6fb84387613
gabriel-reis-silva <gabriel.reis1945@gmail.com>
2021-04-28 03:42:31 -0300

    fix default path to be home

--- Aplicativo/frontend/src/routes/routes.jsx
+++ Aplicativo/frontend/src/routes/routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import UserRegistration from "../pages/UserRegistration";
 import HomeMechanic from "../pages/HomeMechanic";
 import LoginSystem from "../pages/LoginSystem";
@@ -13,6 +13,9 @@ export default function Routes() {
     return (
         <Router>
             <Switch>
+                <Route exact path="/">
+                    <Redirect to="home" />
+                </Route>
                 <Route path="/home" component={Home} />
                 <Route path="/user-list" component={UsersList} />
                 <Route path="/user-registration" component={UserRegistration} />
