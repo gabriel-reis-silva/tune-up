a27d4eccedb7f429260757febef30abd83ba45be
Isabella-santos <isabella.santos@bandtec.com.br>
2021-05-10 18:57:14 -0300

    adding owner and customer registration screen

---
+++ Aplicativo/frontend/src/pages/OwnerRegistration.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import "../assets/css/owner_registration.css";
+import Logo from "../assets/img/Tune-Up.png";
+import Button from "../components/Button";
+export default function UserRegistration() {
+    return (
+        <div className="container_registration">
+            <div className="box_registration">
+                <img className="logo_registration" src={Logo} alt="" />
+                <h2 className="title_registration">CADASTRO DO PROPRIETÁRIO</h2>
+                <form action="" className="form_registration">
+                    <div className="inputs-form_registration">
+                        <div className="inputs_top_registration">
+                            <div className="inputs_left_registration">
+                                <input type="text" placeholder="Nome completo" />
+                                <input type="text" placeholder="CPF" />
+                            </div>
+                            <div className="inputs_right_registration">
+                                <input type="date" placeholder="Data de nascimento" />
+                                <input type="text" placeholder="Telefone" />
+                            </div>
+                        </div>
+                        <div className="input_email">
+                            <input type="text" placeholder="Email" />
+                        </div>
+                        <div className="passwords">
+                            <input type="password" placeholder="Senha" />
+                            <input type="password" placeholder="Confirmação senha" />
+                        </div>
+                        <div className="input-terms_registration">
+                            <input type="checkbox" name="terms" />
+                            <label htmlFor="terms">Li e concordo com os termos de serviço</label>
+                        </div>
+                    </div>
+                    <div className="buttons_register">
+                        <Link className="link_register" to=""> <Button isBackButton={true} classNameButton="btn_back_registration" >Voltar</Button> </Link>
+                        <Link className="link_register" to=""> <Button classNameButton="btn_regitration" >Cadastrar</Button> </Link>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    );
+}
