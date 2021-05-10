816441298696d4c537eb1512dd684877cb2820cd
cesar augusto <cesar.barbosa@bandtec.com.br>
2021-05-10 16:05:28 -0300

    fix/user-pages fixed layout of input in user registration

--- Aplicativo/frontend/src/pages/UserRegistration.jsx
+++ Aplicativo/frontend/src/pages/UserRegistration.jsx
@@ -10,20 +10,26 @@ export default function UserRegistration() {
                 <h2 className="title_registration">Cadastre-se</h2>
                 <form action="" className="form_registration">
                     <div className="inputs-form_registration">
-                        <div className="inputs_left_registration">
-                            <input type="text" placeholder="Nome completo" />
-                            <input type="date" placeholder="Nome completo" />
-                            <input type="text" placeholder="CPF" />
-                            <input type="text" placeholder="Telefone" />
+                        <div className="inputs_top_registration">
+                            <div className="inputs_left_registration">
+                                <input type="text" placeholder="Nome completo" />
+                                <input type="text" placeholder="CPF" />
+                            </div>
+                            <div className="inputs_right_registration">
+                                <input type="date" placeholder="Data de nascimento" />
+                                <input type="text" placeholder="Telefone" />
+                            </div>
                         </div>
-                        <div className="inputs_right_registration">
+                        <div className="input_email">
                             <input type="text" placeholder="Email" />
+                        </div>
+                        <div className="passwords">
                             <input type="text" placeholder="Senha" />
                             <input type="text" placeholder="Confirmação senha" />
-                            <div className="input-terms_registration">
-                                <input type="checkbox" name="terms" />
-                                <label htmlFor="terms">Li e concordo com os termos de serviço</label>
-                            </div>
+                        </div>
+                        <div className="input-terms_registration">
+                            <input type="checkbox" name="terms" />
+                            <label htmlFor="terms">Li e concordo com os termos de serviço</label>
                         </div>
                     </div>
                     <div className="buttons_register">
