449f5f18fd3286f5a8011fc54e737d3c9248868b
gabriel-reis-silva <gabriel.reis1945@gmail.com>
2021-05-14 00:45:07 -0300

    post feature finished for user

--- Aplicativo/frontend/src/pages/UserRegistration.jsx
+++ Aplicativo/frontend/src/pages/UserRegistration.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../assets/css/user_registration.css";
 import Logo from "../assets/img/Tune-Up.png";
 import Button from "../components/Button";
+import api from "../services/api";    
+
 export default function UserRegistration() {
+
+    const [usuario, setUsuario] = useState(
+        {
+                cpf : "",
+                dataNasc : "",
+                email : "",
+                nome : "",
+                senha : "",
+                telefone : ""
+        });
+    
+        async function cadastrar(){
+            const resposta = await api.post("/", {
+              ...usuario,
+            });
+            if(resposta.status === 201){
+              alert("Usuário cadastrado" + resposta.status);
+            }else{
+              alert("erro!" + resposta.status);
+            }
+          }
+    
+        function handleInput(evento) {
+    
+            const { name, value } = evento.target;
+            setUsuario({
+              ...usuario,
+              [name]: value
+            });
+            console.log(usuario);
+        
+          }
+    
+
     return (
         <div className="container_registration">
             <div className="box_registration">
@@ -10,16 +47,16 @@ export default function UserRegistration() {
                 <h2 className="title_registration">Cadastre-se</h2>
                 <form action="" className="form_registration">
                     <div className="inputs_left_registration">
-                        <input type="text" placeholder="Nome completo" />
-                        <input type="date" placeholder="Nome completo" />
-                        <input type="text" placeholder="CPF" />
-                        <input type="text" placeholder="Telefone" />
+                        <input type="text" placeholder="Nome completo" name="nome" onChange={handleInput} required="true"/>
+                        <input type="date" name="dataNasc" onChange={handleInput} required="true"/>
+                        <input type="text" placeholder="CPF" name="cpf"onChange={handleInput} required="true"/>
+                        <input type="text" placeholder="Telefone" name="telefone" onChange={handleInput} required="true"/>
                     </div>
                     <div className="inputs_right_registration">
-                        <input type="text" placeholder="Email" />
-                        <input type="text" placeholder="Senha" />
-                        <input type="text" placeholder="Confirmação senha" />
-                     <Link to=""> <Button classNameButton="btn_regitration" >Cadastrar</Button> </Link>
+                        <input type="email" placeholder="Email" name="email" onChange={handleInput} required="true"/>
+                        <input type="password" placeholder="Senha" name="senha" onChange={handleInput} required="true"/>
+                        <input type="password" placeholder="Confirmação senha"  required="true"/>{/* espaço reservado para que você faça a validação do campo*/}
+                      <button onClick={cadastrar} className="btn_registration">Cadastrar</ button> 
                     </div>
 
                 </form>
