886cfb9a49b7e1d55f80da81ec90593a231e96ba
gabriel-reis-silva <gabriel.reis1945@gmail.com>
2021-05-15 01:42:07 -0300

    oficina controller

---
+++ Aplicativo/backend/ems/src/main/java/com/bandtec/tuneup/br/ecs/repositorio/OficinaRepository.java
@@ -0,0 +1,7 @@
+package com.bandtec.tuneup.br.ecs.repositorio;
+
+import com.bandtec.tuneup.br.ecs.dominio.Oficina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OficinaRepository extends JpaRepository<Oficina, Integer> {
+}
